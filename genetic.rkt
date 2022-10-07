b2e1a8a36bc19f32fc3bba9576646794e89a7bea
Esteban <esteban25agosto@gmail.com>
2022-10-07 15:06:20 -0600

    Algoritmo genetico

---
+++ genetic.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+;; Estructura de los genes:
+'(Velocidad, Fuerza, Posicion, Habilidad) ;; En la primera generacion todos estos atributos seran aleatorios.
+
+;;;; Operaciones
+
+;; Primera generacion
+
+;; Funcion de aptitud (asignacion del "Fitness Score")
