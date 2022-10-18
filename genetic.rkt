101fc7385d469d949634f4e68c8841b9b769f0ac
Esteban <esteban25agosto@gmail.com>
2022-10-17 19:12:08 -0600

    Copia

--- genetic.rkt
+++ genetic.rkt
@@ -1,10 +1,24 @@
 #lang racket
 
 ;; Estructura de los genes:
-'(Velocidad, Fuerza, Posicion, Habilidad) ;; En la primera generacion todos estos atributos seran aleatorios.
+(struct individual '(Velocidad, Fuerza, Posicion, Habilidad)) ;; En la primera generacion todos estos atributos seran aleatorios.
+
+;; Definicion de la primera generacion
+
 
 ;;;; Operaciones
 
 ;; Primera generacion
 
 ;; Funcion de aptitud (asignacion del "Fitness Score")
+
+;; Funcion de seleccion (eleccion de los mejores individuos)
+
+;; Funcion de reproduccion (creacion de hijos con base en los mejores individuos seleccionados)
+
+;; Funcion de mutacion (insertar genes aleatorios)
+
+
+
+(define a (random 0 2))
+a
